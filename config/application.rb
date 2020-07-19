78515415b5998adf3f4f3a567d1b0ee105d6a04c
hironori sasaki <hrnr.ssk@gmail.com>
2020-07-19 12:51:09 +0900

    エラーメッセージの日本語化。model.ja.ymlに追記。

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module Instaclone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # i18n
+    config.i18n.default_locale = :ja
   end
 end
